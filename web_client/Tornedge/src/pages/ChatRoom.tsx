33ffa1f0beee477b300ebe281da975a64b9a561a
Kazuki Okugawa <droitok@gmail.com>
2021-01-02 14:21:23 +0900

    Add Toast

--- web_client/Tornedge/src/pages/ChatRoom.tsx
+++ web_client/Tornedge/src/pages/ChatRoom.tsx
@@ -16,6 +16,7 @@ import {
   IonToolbar,
   useIonViewWillEnter,
   IonInput,
+  IonToast
 } from '@ionic/react';
 import React, { useState } from 'react';
 import './ChatRoom.css';
@@ -112,61 +113,94 @@ const MsgList: React.FC<Messages> = (props) => {
 const ChatRoom: React.FC<RouteComponentProps> = (props) => {
   const [messages, setMessages] = useState(testMessages)
   const [message, setMessage] = useState('')
-  
+  const [showToast, setShowToast] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')  
+
   const handleClickBack = () => {
     console.log('[API] exit_chat');
-    // Function
-    //   Step1: Send ID and command
-    //   Step2: Change ID's chatflag data to false
+   /* Function
+    *   Step1: Send request
+    *   Step2: clear chat log data in state
+    *   Step3: back to ChatTop page
+    */   
+
+    // 1: send request
     const formData = generateFormData(
       'cmd', 'exit_chat',
       'image_id', '1'
     )
-
     axios.post('http://localhost:56060', formData).then((response) => {
-      console.log(response);
+      // 2: clear chat log data in state
+      const res: string = response.data.data.result
+      if(res == 'success') {
+        setMessages([]);
+      } else {
+        setErrorMessage(response.data.data.message);
+        setShowToast(true);
+      }
     })
     .catch((error) => {
       console.log(error);
     })
-
+    // 3: back to ChatTop page
     props.history.goBack(); 
   }
 
-  const handleClickSend = () => {
-    console.log('[API] send_chat');
 
+  const handleClickSend = () => {
+   /* Functions
+    *   Step1: Send request
+    *   Step2: Update chat log value in state
+    *   Step3: Reset value of input textarea
+    */
+    console.log('[API] send_chat');
+    
+    // 1: send request
     const formData = generateFormData(
       'cmd', 'send_chat',
       'message', message,
       'chat_room_id', localStorage.getItem('chat_room_id')
     );
-
-    axios.post('http://localhost:56060', formData).then((respose) => {
-      setMessages(respose.data.data.chat_log);
+    axios.post('http://localhost:56060', formData).then((response) => {
+      // 2: update chat log value in state
+      if(response.data.data.result === 'success') {
+        setMessages(response.data.data.chat_log);
+      } else {
+        setErrorMessage(response.data.data.message);
+        setShowToast(true);
+      }
     })
+    // 3: Reset value of input textarea
     setMessage('');
   }
 
+
   const handleClickUpdate = () => {
+   /* Functions
+    *   Step1: Send request
+    *   Step2: Update chat log value in state
+    */
     console.log('[API] update_chat');
-  }
-
-  useIonViewWillEnter(() => {
-    console.log('[API] enter_chat_room')
-
-    const formData = new FormData();
-    formData.append('cmd', 'enter_chat_room');
-    formData.append('image_id', '1');
-
-    axios.post('http://localhost:56060', formData).then((response) => {
-      console.log(response.data);
+    // 1: send request
+    const formData = generateFormData(
+      'cmd', 'update_chat',
+      'chat_room_id', localStorage.getItem('chat_room_id')
+    )
+    axios.post('http:/localhost:56060', formData).then((response) => {
+      // 2: update chat log value in state
+      if(response.data.data.reslut == 'success') {
+        setMessages(response.data.data.chat_log);
+      } else {
+        setErrorMessage(response.data.data.message);
+        setShowToast(true);
+      }
     })
     .catch((error) => {
       console.log(error);
-    });
-    
-  })
+    })
+  }
+
+  useIonViewWillEnter(() => handleClickUpdate())
 
   return (
     <IonPage>
@@ -213,7 +247,13 @@ const ChatRoom: React.FC<RouteComponentProps> = (props) => {
         <IonButton color="success" expand="block" onClick={() => handleClickSend()}>Send</IonButton>
       </div>
 
-
+      {/* Toast */}
+      <IonToast
+        isOpen={showToast}
+        onDidDismiss={() => setShowToast(false)}
+        message={errorMessage}
+        duration={200}
+      />
 
     </IonPage>
   );
